12ffb2f926d53dddd4e0a8b4c68204f26bf218c3
David Minaya <dminaya@intellisys.com.do>
2019-08-14 11:36:57 -0400

    Remove unnecessary code

--- index.js
+++ index.js
@@ -81,52 +81,25 @@ function getAccessToken(oAuth2Client, callback) {
 }
 
 /**
- * Creates a new script project, upload a file, and log the script's URL.
+ * Call a Apps Script Project.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function callAppsScript(auth) {
+  
   const script = google.script({version: 'v1', auth});
-  /*script.projects.create({
-    resource: {
-      title: 'My Script',
-    },
-  }, (err, res) => {
-    if (err) return console.log(`The API create method returned an error: ${err}`);
-    script.projects.updateContent({
-      scriptId: res.data.scriptId,
-      auth,
-      resource: {
-        files: [{
-          name: 'hello',
-          type: 'SERVER_JS',
-          source: 'function helloWorld() {\n  console.log("Hello, world!");\n}',
-        }, {
-          name: 'appsscript',
-          type: 'JSON',
-          source: '{\"timeZone\":\"America/New_York\",\"exceptionLogging\":' +
-           '\"CLOUD\"}',
-        }],
-      },
-    }, {}, (err, res) => {
-      if (err) return console.log(`The API updateContent method returned an error: ${err}`);
-      console.log(`https://script.google.com/d/${res.data.scriptId}/edit`);*/
+      
+  console.log('Run the script')
 
-      // Run the script
+  const scriptId = '1CyemDoDsvclC4gAe49XKpQfm6E_k7GoDWwMB6KdEamT1Yh_LNghiFVSu'
 
-      console.log('Run the script')
-
-      const scriptId = '1CyemDoDsvclC4gAe49XKpQfm6E_k7GoDWwMB6KdEamT1Yh_LNghiFVSu'
-
-      script.scripts.run({
-          'scriptId': scriptId,
-          'resource': {
-              'function': 'HelloWorld'
-          }
-      }).then(res => {
-          console.log('>>>>>>>')
-          console.log(res.data)
-          console.log('<<<<<<<<<')
-      })
-    /*});
-  });*/
+  script.scripts.run({
+    'scriptId': scriptId,
+    'resource': {
+      'function': 'HelloWorld'
+    }
+  }).then(res => {
+    console.log('>>>>>>>')
+    console.log(res.data)
+    console.log('<<<<<<<<<')
+  })
 }
